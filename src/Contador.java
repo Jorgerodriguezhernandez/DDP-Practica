b157311b64f64e803505a1655e024dbf66a8c2dc
Jorge Rodriguez Hernandez <jorgexarach298@gmail.com>
2021-05-13 13:04:06 +0100

    Falla Creacion de objeto

---
+++ src/Contador.java
@@ -0,0 +1,12 @@
+public class Contador{
+
+    int valorInicial = 0;
+    int valorFinal = 0;
+    int valorIncremento = 0;
+
+
+
+
+
+
+}
---
+++ src/ContadorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ContadorTest {
+    @Test
+    public void testCreacionObjet() {
+
+        Contador calculo = new Contador(1,6,1);
+        
+    }
+
+}
